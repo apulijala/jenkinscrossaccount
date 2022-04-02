b9f4c9b3544ce7921b9125cc3bc0df6fdec9f6af
arvind <apulijala>
2022-04-02 16:52:13 +0100

    adding new shared library

--- vars/terraformApply.groovy
+++ vars/terraformApply.groovy
@@ -9,19 +9,25 @@ def call(todo, rolearn, whatodo) {
 }
 
 def terraform_plan(todo,rolearn) {
-    echo "In terraform plan"
+    echo "In the call method"
+    withAWS(region: 'eu-west-1', role: rolearn) {
+        if (todo == "Apply") {
+            sh "terraform plan -no-color"
+        }
+        else {
+            sh "terraform plan -destroy -no-color"
+        }
+    }
 }
 
 def terraform_apply(todo,rolearn) {
-
-    echo "In the call method"
     withAWS(region: 'eu-west-1', role: rolearn) {
-    if (todo == "Apply") {
-        sh "terraform apply -auto-approve -no-color"
+        if (todo == "Apply") {
+            sh "terraform apply -auto-approve -no-color"
+        }
+        else {
+            sh "terraform destroy -auto-approve -no-color"
+        }
     }
-    else {
-        sh "terraform destroy -auto-approve -no-color"
-    }
-}
 
 }
